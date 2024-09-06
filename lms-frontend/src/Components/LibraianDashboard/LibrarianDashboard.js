205878d7b3ca0ab30d29c514b02349263ec3d0b2
JRebadulla123 <jellianrebadulla@gmail.com>
2024-09-06 11:34:44 +0800

    Create LibraianDashboard

---
+++ lms-frontend/src/Components/LibraianDashboard/LibrarianDashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './LibrarianDashboard.css';
+
+const LibrarianDashboard = () => {
+
+    return (
+<p>Welcome to Librarian Dashboard</p>
+    );
+}
+
+export default LibrarianDashboard;
